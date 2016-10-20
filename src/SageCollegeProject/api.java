56d39c7ea676f434813baeadd54b2ab7164ef80f
skyea <skyea@DESKTOP-NF5FJHF>
2016-10-20 10:08:21 -0500

    Added zipped project

---
+++ src/SageCollegeProject/api.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SageCollegeProject;
+
+/**
+ *
+ * @author SageCollegeProject
+ */
+public class api {
+    
+    private static String Version="Alpha_1.04";
+    
+    public static String GetVersion()
+    {
+    return Version;
+    }
+
+}
