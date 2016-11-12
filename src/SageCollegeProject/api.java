1b7c45bdd9ecebedc74e69838c908e8aef5881a9
skyea <skyea@DESKTOP-NF5FJHF>
2016-11-11 18:11:53 -0600

    Many fixes.
    Fixed next episode screen from looping
    Fixed next episode screen play next episode to work
    Fixed focus coming back from other menus to not be on submenu
    Fixed focus wouldn't set on show if you had watched last episode

--- src/SageCollegeProject/api.java
+++ src/SageCollegeProject/api.java
@@ -13,7 +13,7 @@ package SageCollegeProject;
  */
 public class api {
     
-    private static String Version="1.26";
+    private static String Version="1.27";
     
     public static void main(String[] args) 
     {
