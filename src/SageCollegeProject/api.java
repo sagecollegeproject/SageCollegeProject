d0d72205827b4219775844a9206d57280922cdee
skyea <skyea@DESKTOP-NF5FJHF>
2016-11-17 19:04:14 -0600

    STVi 1.23 Fixed issue with extra scroll on main menu Added letter jumper if property viewnameLetterJump=true Fixed large letters on options menu not showing fully Upgraded vfs file for new favorites view
    API
    adders in metadata/menu needed for letter jump

--- src/SageCollegeProject/api.java
+++ src/SageCollegeProject/api.java
@@ -13,7 +13,7 @@ package SageCollegeProject;
  */
 public class api {
     
-    private static String Version="1.32";
+    private static String Version="1.33";
     
     public static void main(String[] args) 
     {
